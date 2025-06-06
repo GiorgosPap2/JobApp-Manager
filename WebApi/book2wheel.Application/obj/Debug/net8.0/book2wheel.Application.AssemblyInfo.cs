d985587bc754637affcd74fd7fa71a0bc2853aa8
Giorgos Papadopoulos <gio2003papa@gmail.com>
2025-06-06 17:51:23 +0300

    building oproject

--- WebApi/book2wheel.Application/obj/Debug/net8.0/book2wheel.Application.AssemblyInfo.cs
+++ WebApi/book2wheel.Application/obj/Debug/net8.0/book2wheel.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("book2wheel.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0348a79240e182204d0899a2aac12d03d59c53d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b002d0249eaa56db932a4382dc73eae89c4dfaa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("book2wheel.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("book2wheel.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
